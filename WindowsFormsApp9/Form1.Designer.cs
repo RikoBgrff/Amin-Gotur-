50566e6f2a8730ab25aaaf47b091e7c59173574e
Bagirli Arifali Azad <Bagi_mz73@itstep.edu.az>
2021-03-28 15:30:18 +0400

    Add project files.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button4 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button5 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button6 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button7 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button8 = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(651, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(12, 20);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button1.TabIndex = 1;
+            this.guna2Button1.Text = "Line";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(12, 71);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button2.TabIndex = 2;
+            this.guna2Button2.Text = "Circle";
+            this.guna2Button2.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button3
+            // 
+            this.guna2Button3.CheckedState.Parent = this.guna2Button3;
+            this.guna2Button3.CustomImages.Parent = this.guna2Button3;
+            this.guna2Button3.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button3.ForeColor = System.Drawing.Color.White;
+            this.guna2Button3.HoverState.Parent = this.guna2Button3;
+            this.guna2Button3.Location = new System.Drawing.Point(12, 122);
+            this.guna2Button3.Name = "guna2Button3";
+            this.guna2Button3.ShadowDecoration.Parent = this.guna2Button3;
+            this.guna2Button3.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button3.TabIndex = 3;
+            this.guna2Button3.Text = "Rectangle";
+            this.guna2Button3.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button4
+            // 
+            this.guna2Button4.CheckedState.Parent = this.guna2Button4;
+            this.guna2Button4.CustomImages.Parent = this.guna2Button4;
+            this.guna2Button4.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button4.ForeColor = System.Drawing.Color.White;
+            this.guna2Button4.HoverState.Parent = this.guna2Button4;
+            this.guna2Button4.Location = new System.Drawing.Point(12, 224);
+            this.guna2Button4.Name = "guna2Button4";
+            this.guna2Button4.ShadowDecoration.Parent = this.guna2Button4;
+            this.guna2Button4.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button4.TabIndex = 5;
+            this.guna2Button4.Text = "Arc";
+            this.guna2Button4.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button5
+            // 
+            this.guna2Button5.CheckedState.Parent = this.guna2Button5;
+            this.guna2Button5.CustomImages.Parent = this.guna2Button5;
+            this.guna2Button5.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button5.ForeColor = System.Drawing.Color.White;
+            this.guna2Button5.HoverState.Parent = this.guna2Button5;
+            this.guna2Button5.Location = new System.Drawing.Point(12, 173);
+            this.guna2Button5.Name = "guna2Button5";
+            this.guna2Button5.ShadowDecoration.Parent = this.guna2Button5;
+            this.guna2Button5.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button5.TabIndex = 4;
+            this.guna2Button5.Text = "Polygon";
+            this.guna2Button5.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button6
+            // 
+            this.guna2Button6.CheckedState.Parent = this.guna2Button6;
+            this.guna2Button6.CustomImages.Parent = this.guna2Button6;
+            this.guna2Button6.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button6.ForeColor = System.Drawing.Color.White;
+            this.guna2Button6.HoverState.Parent = this.guna2Button6;
+            this.guna2Button6.Location = new System.Drawing.Point(12, 326);
+            this.guna2Button6.Name = "guna2Button6";
+            this.guna2Button6.ShadowDecoration.Parent = this.guna2Button6;
+            this.guna2Button6.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button6.TabIndex = 7;
+            this.guna2Button6.Text = "Text";
+            this.guna2Button6.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button7
+            // 
+            this.guna2Button7.CheckedState.Parent = this.guna2Button7;
+            this.guna2Button7.CustomImages.Parent = this.guna2Button7;
+            this.guna2Button7.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button7.ForeColor = System.Drawing.Color.White;
+            this.guna2Button7.HoverState.Parent = this.guna2Button7;
+            this.guna2Button7.Location = new System.Drawing.Point(12, 275);
+            this.guna2Button7.Name = "guna2Button7";
+            this.guna2Button7.ShadowDecoration.Parent = this.guna2Button7;
+            this.guna2Button7.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button7.TabIndex = 6;
+            this.guna2Button7.Text = "Curve";
+            this.guna2Button7.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2Button8
+            // 
+            this.guna2Button8.CheckedState.Parent = this.guna2Button8;
+            this.guna2Button8.CustomImages.Parent = this.guna2Button8;
+            this.guna2Button8.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button8.ForeColor = System.Drawing.Color.White;
+            this.guna2Button8.HoverState.Parent = this.guna2Button8;
+            this.guna2Button8.Location = new System.Drawing.Point(12, 377);
+            this.guna2Button8.Name = "guna2Button8";
+            this.guna2Button8.ShadowDecoration.Parent = this.guna2Button8;
+            this.guna2Button8.Size = new System.Drawing.Size(88, 45);
+            this.guna2Button8.TabIndex = 8;
+            this.guna2Button8.Text = "Image";
+            this.guna2Button8.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guna2Button8);
+            this.Controls.Add(this.guna2Button6);
+            this.Controls.Add(this.guna2Button7);
+            this.Controls.Add(this.guna2Button4);
+            this.Controls.Add(this.guna2Button5);
+            this.Controls.Add(this.guna2Button3);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Button guna2Button3;
+        private Guna.UI2.WinForms.Guna2Button guna2Button4;
+        private Guna.UI2.WinForms.Guna2Button guna2Button5;
+        private Guna.UI2.WinForms.Guna2Button guna2Button6;
+        private Guna.UI2.WinForms.Guna2Button guna2Button7;
+        private Guna.UI2.WinForms.Guna2Button guna2Button8;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,117 @@
+﻿using Guna.UI2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+
+    public partial class Form1 : Form
+    {
+        Graphics graphics_context;
+        Pen outline;
+        SolidBrush fillColor;
+        public Form1()
+        {
+            InitializeComponent();
+            graphics_context = this.CreateGraphics();
+            outline = new Pen(Color.Red, 2);
+            fillColor = new SolidBrush(Color.Green);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label1.Text = $"X:{e.X} Y:{e.Y}";
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            if (sender is Guna2Button btn)
+            {
+                switch (btn.Text)
+                {
+                    case "Line":
+                        {
+                            //graphics_context.DrawLine(outline, 150, 10, 250, 100);
+                            break;
+                        }
+                    case "Circle":
+                        {
+                            //graphics_context.DrawEllipse(outline, 300, 10, 50, 50);
+                            //graphics_context.FillEllipse(fillColor, 300, 10, 50, 50);
+                            break;
+                        }
+                    case "Rectangle":
+                        {
+                            //graphics_context.DrawRectangle(outline, 300, 10, 50, 50);
+                            //graphics_context.FillRectangle(fillColor, 300, 10, 50, 50);
+                            break;
+                        }
+                    case "Polygon":
+                        {
+                            Point[] points = new Point[5]
+                            {
+                                new Point(160,120),
+                                new Point(120,180),
+                                new Point(190,160),
+                                new Point(200,241),
+                                new Point(214,180),
+                            };
+                            graphics_context.FillPolygon(fillColor, points);
+                            break;
+                        }
+                    case "Arc":
+                        {
+                            Rectangle rectangle = new Rectangle(450, 50, 100, 100);
+                            graphics_context.DrawRectangle(outline, 450, 50, 160, 100);
+                            graphics_context.DrawArc(outline, rectangle, 90, 180);
+                            break;
+                        }
+                    case "Curve":
+                        {
+                            Point[] points = new Point[5]
+                            {
+                                new Point(250,220),
+                                new Point(220,280),
+                                new Point(290,280),
+                                new Point(300,341),
+                                new Point(314,280),
+                            };
+
+                            graphics_context.FillClosedCurve(fillColor, points);
+                            break;
+                        }
+                    case "Text":
+                        {
+                            Font font = new Font("Verdana", 20);
+                            Point location = new Point(400, 200);
+                            StringFormat drawFormat = new StringFormat();
+                            drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+                            graphics_context.DrawString("Hello World", font, fillColor, location, drawFormat);
+                            break;
+                        }
+                    case "Image":
+                        {
+                            PictureBox pictureBox = new PictureBox();
+                            pictureBox.Location = 
+                            break;
+                        }
+                    default:
+                        MessageBox.Show("Not Found");
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
